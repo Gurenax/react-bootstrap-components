d1f6f23988a1a42aa32bc7e3c858c67ebd473e9e
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-11-27 20:29:10 +1100

    Add comments to Form Group component

--- src/components/forms/FormGroup.js
+++ src/components/forms/FormGroup.js
@@ -1,5 +1,10 @@
 import React from 'react'
 
+/*
+*   FormGroup component
+*   children - any component under FormGroup
+*   type - type of form group (default: group)
+*/
 const FormGroup = ({ children, type='group' }) => {
   let formGroupType = `form-${type}`
   return (
