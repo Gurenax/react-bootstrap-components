cd54fd6163aca1f921142618d5a48e471d4df454
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-11-27 20:25:42 +1100

    Refactor and add comments to Form component

--- src/components/forms/Form.js
+++ src/components/forms/Form.js
@@ -1,7 +1,13 @@
 import React from 'react'
 
-const Form = ({ children, action, method='POST' }) => ( 
-  <form action={action} method={method}>
+/*
+*   Form component
+*   children - any component under Form component
+*   url - url where the form will be submitted
+*   method - submit method i.e. GET or POST
+*/
+const Form = ({ children, url, method='POST' }) => ( 
+  <form action={url} method={method}>
     { children }
   </form>
 )
