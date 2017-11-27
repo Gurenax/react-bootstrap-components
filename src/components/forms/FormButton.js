52838dc27ef1e506af561ca81dc27f7705830009
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-11-27 20:56:43 +1100

    Refactor FormButton component's  title

--- src/components/forms/FormButton.js
+++ src/components/forms/FormButton.js
@@ -3,16 +3,16 @@ import React from 'react'
 /*
 *   Form Button component
 *   type - button type e.g. submit or reset
-*   children - button text
+*   title - button text
 *   variation - button color e.g. success, danger, warning
 */
-const FormButton = ({ type='submit', children='Submit', variation='primary' }) => {
+const FormButton = ({ type='submit', title='Submit', variation='primary' }) => {
   let className = `btn`
   if (variation) {
     className += ` btn-${variation}`
   }
   return (
-    <button type={ type } className={className}>{ children }</button>
+    <button type={ type } className={className}>{ title }</button>
   )
 }
 
