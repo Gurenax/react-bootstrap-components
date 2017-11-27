fe4ca37f983a31545487cce85c12c0ab61ff7123
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-11-27 20:30:47 +1100

    Add comments to Label component

--- src/components/forms/Label.js
+++ src/components/forms/Label.js
@@ -1,5 +1,10 @@
 import React from 'react'
 
+/*
+*   Label component
+*   forId - id of target component to be labeled
+*   children - any component under label (e.g. text for label)
+*/
 const Label = ({ forId, children }) => ( 
   <label for={ forId }>{ children }</label>
 )
