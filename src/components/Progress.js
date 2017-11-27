b15abe3711c1bb4ba5d269040a11c55dc8a39a89
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-11-27 20:35:33 +1100

    Add comments to Progress component

--- src/components/Progress.js
+++ src/components/Progress.js
@@ -1,5 +1,12 @@
 import React from 'react'
 
+/*
+*   Progress component
+*   value - progress value out of 100
+*   striped - enable to add striped design to progress bar
+*   animated - enable to add animation to progress bar
+*   color - color of the progress bar e.g. success, info, warning, danger
+*/
 const Progress = ({ value, striped, animated, color }) => {
   let percent = value+'%'
   let className = 'progress-bar'
